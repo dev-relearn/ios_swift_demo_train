e2333540fe7fff5e5c3dd37330c27c853b49b1a1
Athira <athira.krishnan0@gmail.com>
2024-01-26 19:05:01 +0100

    Initial Commit

---
+++ DemoTrain/DemoTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTrainApp.swift
+//  DemoTrain
+//
+//  Created by Athira Krishnan on 26.01.24.
+//
+
+import SwiftUI
+
+@main
+struct DemoTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
